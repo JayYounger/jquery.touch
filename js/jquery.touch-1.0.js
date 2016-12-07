8747ccf5946408bf7207c06e5db56c162402a835
JayYounger <www.exam@qq.com>
2016-12-07 17:10:26 +0800

    为jQuery提供滑动事件回调
    为jQuery提供swipeLeft,swipeRight,swipeUp,swipeDown等事件

---
+++ js/jquery.touch-1.0.js
@@ -0,0 +1,34 @@
+﻿(function(){
+    function swipeDirection(x1, x2, y1, y2) {
+        return Math.abs(x1 - x2) >=
+            Math.abs(y1 - y2) ? (x1 - x2 > 0 ? 'Left' : 'Right') : (y1 - y2 > 0 ? 'Up' : 'Down')
+    }
+    $(document).ready(function(){
+        var touch = {};
+        $(document)
+            .on('touchstart',function(e){
+                e = e.originalEvent ? e.originalEvent : e;
+                var fTouch = e.touches[0];
+                touch.x1 = fTouch.pageX;
+                touch.y1 = fTouch.pageY;
+                touch.el = $('tagName' in fTouch.target ?
+                    fTouch.target : fTouch.target.parentNode);
+            })
+            .on('touchmove',function(e){
+                e = e.originalEvent ? e.originalEvent : e;
+                var fTouch = e.touches[0];
+                touch.x2 = fTouch.pageX;
+                touch.y2 = fTouch.pageY;
+            })
+            .on('touchend',function(e){
+                if ((touch.x2 && Math.abs(touch.x1 - touch.x2) > 30) ||
+                    (touch.y2 && Math.abs(touch.y1 - touch.y2) > 30)){
+                    touch.el.trigger('swipe' + (swipeDirection(touch.x1, touch.x2, touch.y1, touch.y2)))
+                    touch = {}
+                }
+            });
+    });
+    ["swipeLeft","swipeRight","swipeUp","swipeDown"].forEach(function(eName){
+        $.fn[eName] = function(callback){ return this.on(eName, callback) }
+    });
+})($);
